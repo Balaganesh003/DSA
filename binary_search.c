e947a17a6467c0df714d335a4915060ba95942ed
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2022-09-14 05:35:55 +0530

    Updated comments, rename to binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+// List has to be sorted before using binary search 
 
 int BinarySearch( int arr[],int x, int low,int high)
 {
@@ -19,8 +20,8 @@ int BinarySearch( int arr[],int x, int low,int high)
 int main()
 {
     int arr1[]={1,2,3,4,5,6,7,8,9,10};
-    int x=5;
-    int low=0;
+    int x=5; // element to find
+    int low=0; 
     int high=9;  // size of array -1
     printf("%d",BinarySearch(arr1,x,low,high));
     return 0;
